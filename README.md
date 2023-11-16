f2bc169b3d7c8d7e2fdae9b1b5aabe32ffc9567a
Piotr Modrakowski <253021@student.pwr.edu.plcd C:\Users\Piotr.DESKTOP-CAEVJSC\Desktop\Semestr 4\PPSgit initgit remote add origin https://zts.ita.pwr.wroc.pl/gitlab/Piotrek/pps-lab.gitgit add .git commit -m Initial>
2023-11-16 13:30:45 +0100

    Script with url argument

---
+++ README.md
@@ -0,0 +1,8 @@
+#AWS S3 Bucket downloading script
+##Description
+This script download AWS S3 Bucket content via URL.
+##How To Use
+It is simple, all you need is url to S3 bucket
+```bash
+python main.py url_to_S3bucket
+```
--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-## https://dsecure.s3-ap-southeast-1.amazonaws.com/
-#!/usr/bin/python
-
 import sys
 import urllib.request
 import re
 import requests
 
-page = urllib.request.urlopen('https://dsecure.s3-ap-southeast-1.amazonaws.com/')
+page = urllib.request.urlopen(sys.argv[1])
 html_page = page.read().decode("utf-8")
 
 pattern = "<Key.*?>.*?</Key.*?>"
 match_results = re.findall(pattern, html_page)
-main_url = 'https://dsecure.s3-ap-southeast-1.amazonaws.com/'
 url_list = []
 for x in match_results:
-	x = re.sub("<.*?>","", x )
-	x = main_url + x
+	x = re.sub("<.*?>", "", x)
+	x = sys.argv[1] + x
 	if requests.get(x).status_code == 200:
 		url = x
 		r = requests.get(x, allow_redirects=True)
 		if url.find('/'):
-			filename=(url.rsplit('/', 1)[1])
+			filename = (url.rsplit('/', 1)[1])
 			open(filename, 'wb').write(r.content)
